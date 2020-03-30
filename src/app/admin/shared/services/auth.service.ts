73659f0acdb98ef8ded174f3610084cd71e4b4b9
pylyp <pylyp.kutov@gmail.com>
2020-03-30 19:04:15 +0200

    add login error handler

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { IUser, IFireBaseAuthResponse } from 'src/app/shared/interfaces';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError, Subject } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthService {
 
+  public error$: Subject<string> = new Subject<string>();
   constructor(private http: HttpClient) {}
 
   get token(): string {
@@ -23,7 +24,8 @@ export class AuthService {
     user.returnSecureToken = true;
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       )
   }
 
@@ -32,7 +34,25 @@ export class AuthService {
   }
 
   isAuth(): boolean{
-    return !!''
+    return !!this.token
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const { message } = error.error.error;
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Invalid email ');
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Invalid password ');
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Email not found');
+        break
+    }
+
+    return throwError(error)
   }
 
   private setToken(response: IFireBaseAuthResponse | null){
